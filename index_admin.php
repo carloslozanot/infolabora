cae36c811a5050c08b2c403b1e82ad795e0842d2
carloslozanot <carlos.lozano@databiz.co>
2025-06-06 14:57:53 -0500

    fg

--- index_admin.php
+++ index_admin.php
@@ -30,7 +30,7 @@ if (!isset($_SESSION['usuario'])) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="/docs/js/script.js"></script>
+    <script src="docs/js/script.js"></script>
 
     <link rel="stylesheet" href="docs/css/estilos.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
