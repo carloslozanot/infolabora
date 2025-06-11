00810e15c6f275389e4d9d4a26356f8b2989a188
carloslozanot <carlos.lozano@databiz.co>
2025-06-11 17:03:28 -0500

    Update certificado.php

--- fpdf/certificado.php
+++ fpdf/certificado.php
@@ -16,7 +16,7 @@ if ($consulta_info->num_rows > 0) {
    $tipo_contrato = $dato_info->tipo_contrato;
    $salario = $dato_info->salario;
    $auxilio = $dato_info->auxilio;
-   $dia = date('d');
+   $fecha_actual = date('d') . ' de ' . date('F') . ' del ' . date('Y');
 } else {
    $nombre_completo = "No disponible";
    $cargo = "No disponible";
