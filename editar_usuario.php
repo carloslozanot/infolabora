692e8b511176d816e802fae44a846c63f4d9cb9d
carloslozanot <carlos.lozano@databiz.co>
2025-07-14 17:46:59 -0500

    estilo

--- editar_usuario.php
+++ editar_usuario.php
@@ -65,7 +65,7 @@ include("php/conexion.php");
         ?>
 
         <div class="titulo-editar-usuario">
-        <p style="font-size: 32px; font-weight:1000;">EDITAR USUARIO<br></p>
+        <p style="font-size: 36px; font-weight:1000;">EDITAR USUARIO<br></p>
         </div>
         <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
 
