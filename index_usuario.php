0864de91fd67ac35d56979c8510b7518b6083c0d
carloslozanot <carlos.lozano@databiz.co>
2025-07-15 09:17:37 -0500

    Update index_usuario.php

--- index_usuario.php
+++ index_usuario.php
@@ -167,7 +167,7 @@ if (!isset($_SESSION['usuario'])) {
                     <div class="grupo-dato">
                         <div class="icono-con-texto">
                             <div class="icono-circular">
-                                <i class="bi bi-file-earmark-person"></i>
+                                <i class="bi bi-pin-map-fill"></i>
                             </div>
                             <div class="texto">
                                 <p class="etiqueta-dato">CIUDAD RESIDENCIA</p>
