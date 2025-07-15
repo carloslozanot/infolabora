a78223372e17f02b93230838ff75b8fce912582e
carloslozanot <carlos.lozano@databiz.co>
2025-07-15 09:19:14 -0500

    Update index_usuario.php

--- index_usuario.php
+++ index_usuario.php
@@ -112,7 +112,7 @@ if (!isset($_SESSION['usuario'])) {
                     <div class="grupo-dato">
                         <div class="icono-con-texto">
                             <div class="icono-circular">
-                                <i class="bi bi-file-earmark-person"></i>
+                                <i class="bi bi-at"></i>
                             </div>
                             <div class="texto">
                                 <p class="etiqueta-dato">CORREO CORPORATIVO</p>
