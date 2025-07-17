5552a4b1a5bdcb9e95d8a1bfb7ddcf0f56dad4bd
carloslozanot <carlos.lozano@databiz.co>
2025-07-17 17:16:17 -0500

    Update index_th.php

--- index_th.php
+++ index_th.php
@@ -34,7 +34,7 @@ $result = mysqli_query($conexion, $sql);
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="docs/css/estilos.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 
