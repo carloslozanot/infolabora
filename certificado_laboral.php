8647f076b432720de890ebba0d9dbfffc2ffa6cb
carloslozanot <carlos.lozano@databiz.co>
2025-07-21 11:42:50 -0500

    Update certificado_laboral.php

--- certificado_laboral.php
+++ certificado_laboral.php
@@ -23,9 +23,9 @@ $cedula = $_SESSION['usuario'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Certificado Laboral</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">    
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
     <link rel="stylesheet" href="docs/css/estilos.css">
@@ -40,34 +40,46 @@ $cedula = $_SESSION['usuario'];
             <!-- Título personal -->
             <label style="font-weight: 600;">Título personal:</label><br>
             <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="titulo" id="senor" value="Señor" required onchange="toggleDestinatario()">
+                <input class="form-check-input" type="radio" name="titulo" id="senor" value="Señor" required
+                    onchange="toggleDestinatario()">
                 <label class="form-check-label" for="senor">Señor</label>
             </div>
             <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="titulo" id="senora" value="Señora" onchange="toggleDestinatario()">
+                <input class="form-check-input" type="radio" name="titulo" id="senora" value="Señora"
+                    onchange="toggleDestinatario()">
                 <label class="form-check-label" for="senora">Señora</label>
             </div>
             <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="titulo" id="senores" value="Señores" onchange="toggleDestinatario()">
+                <input class="form-check-input" type="radio" name="titulo" id="senores" value="Señores"
+                    onchange="toggleDestinatario()">
                 <label class="form-check-label" for="senores">Señores</label>
             </div>
             <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="titulo" id="blanco" value="En blanco" onchange="toggleDestinatario()">
+                <input class="form-check-input" type="radio" name="titulo" id="blanco" value="En blanco"
+                    onchange="toggleDestinatario()">
                 <label class="form-check-label" for="blanco">En blanco</label>
             </div>
 
+            <label style="font-weight: 600;">¿Desea presentar su salario?</label><br>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="titulo" id="salario_si" value="salario_si" required
+                    onchange="toggleDestinatario()">
+                <label class="form-check-label" for="salario_si">Si </label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="titulo" id="salario_no" value="salario_no"
+                    onchange="toggleDestinatario()">
+                <label class="form-check-label" for="salario_no">No</label>
+            </div>
             <br><br>
 
-            <!-- Campo destinatario -->
             <label style="font-weight: 600;">Dirigido a:</label>
             <input type="text" id="destinatario" name="destinatario" class="form-control"
-                   style="width: 350px; height: 30px;" placeholder="Escriba a quién va dirigido" required>
+                style="width: 350px; height: 30px;" placeholder="Escriba a quién va dirigido" required>
 
-            <!-- Campo oculto con la cédula -->
             <input type="hidden" name="id" value="<?php echo $cedula; ?>">
 
             <br>
-            <!-- Botón para generar -->
             <button type="submit" class="btn boton-certificado">
                 <i class="fa-solid fa-file"></i> GENERAR CERTIFICADO
             </button>
@@ -78,7 +90,7 @@ $cedula = $_SESSION['usuario'];
         function toggleDestinatario() {
             const radios = document.getElementsByName('titulo');
             const destinatario = document.getElementById('destinatario');
-            
+
             let seleccionado = '';
             for (let i = 0; i < radios.length; i++) {
                 if (radios[i].checked) {
