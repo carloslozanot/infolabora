ac77073fa704e489e20b871aed31fc111f7ea435
carloslozanot <carlos.lozano@databiz.co>
2025-07-24 18:47:48 -0500

    b

--- certificado_laboral.php
+++ certificado_laboral.php
@@ -33,7 +33,7 @@ $cedula = $_SESSION['usuario'];
 </head>
 
 <body>
-    <div id="contenido-certificados">
+    <div id="contenido-certificados-lab">
         <div class="card shadow-lg p-4 seccion-certificados text-center">
             <h2 style="font-size: 35px; font-weight: 700;">CERTIFICADO LABORAL</h2>
 
