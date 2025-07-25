307ca1cab9a7cfea51765bd07eaf6eee89027d06
carloslozanot <carlos.lozano@databiz.co>
2025-07-25 13:20:45 -0500

    Update solicitar_vacaciones.php

--- solicitar_vacaciones.php
+++ solicitar_vacaciones.php
@@ -17,10 +17,17 @@ if (!isset($_SESSION['usuario'])) {
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Solicitud Vacaciones</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="docs/css/estilos.css">
+    <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
 </head>
 
 <script>
@@ -49,7 +56,7 @@ if (!isset($_SESSION['usuario'])) {
 </script>
 
 <body>
-    <div class="container mt-5" id="agregar-solicitud">
+    <div id="agregar-solicitud">
         <?php
         include("php/conexion.php");
 
