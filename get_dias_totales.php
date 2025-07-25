c80833c3465087198b2c161e2f932b71a2286b56
carloslozanot <carlos.lozano@databiz.co>
2025-07-25 13:22:41 -0500

    Update get_dias_totales.php

--- get_dias_totales.php
+++ get_dias_totales.php
@@ -1,4 +1,19 @@
 <?php
-echo "CEDULA: " . ($_GET['cedula'] ?? 'NO LLEGA') . "<br>";
-echo "PERIODO: " . ($_GET['periodo'] ?? 'NO LLEGA');
+include("php/conexion.php");
+
+$cedula = $_GET['cedula'] ?? '';
+$periodo = $_GET['periodo'] ?? '';
+
+if ($cedula && $periodo) {
+    $sql = "SELECT dias_totales FROM vacaciones WHERE cedula = '$cedula' AND periodo = '$periodo' LIMIT 1";
+    $resultado = mysqli_query($conexion, $sql);
+
+    if ($row = mysqli_fetch_assoc($resultado)) {
+        echo $row['dias_totales'];
+    } else {
+        echo '0';
+    }
+} else {
+    echo '0';
+}
 ?>
