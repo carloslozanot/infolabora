577503aaae105b204b7d1aa5b384b97f86482322
carloslozanot <carlos.lozano@databiz.co>
2025-07-28 12:36:10 -0500

    Update exportar_excel_bitacora.php

--- exportar_excel_bitacora.php
+++ exportar_excel_bitacora.php
@@ -2,7 +2,7 @@
 require 'php/conexion.php';
 
 header("Content-Type: application/vnd.ms-excel");
-header("Content-Disposition: attachment; filename=bitacora.xls");
+header("Content-Disposition: attachment; filename=bitacora.xlsx");
 header("Pragma: no-cache");
 header("Expires: 0");
 
