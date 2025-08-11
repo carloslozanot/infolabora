e1f38413ae7fa2386869582344df28ff5261dbe6
carloslozanot <carlos.lozano@databiz.co>
2025-08-11 16:03:08 -0500

    Update editar_integrante.php

--- editar_integrante.php
+++ editar_integrante.php
@@ -1,41 +1,8 @@
-<?php
-session_start();
-
-include("php/conexion.php");
-
-$permiso = $_SESSION['permiso'] ?? null;
-
-if ($permiso == '1') {
-    $destino = 'index_admin.php';
-} else {
-    $destino = 'index_th.php';
-}
-?>
-
-
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar Integrante</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="docs/css/estilos.css">
-    <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
-</head>
-
-
 <div id="contenido-carga">
     <div class="container py-5">
         <div class="row g-4 justify-content-center">
 
+            <!-- Tarjeta 1 -->
             <div class="col-12 col-md-6 col-lg-4">
                 <div class="tarjeta tarjeta-hover shadow-lg border-0 text-center">
                     <div class="tarjeta-body">
@@ -45,6 +12,7 @@ if ($permiso == '1') {
                 </div>
             </div>
 
+            <!-- Tarjeta 2 -->
             <div class="col-12 col-md-6 col-lg-4">
                 <div class="tarjeta tarjeta-hover shadow-lg border-0 text-center">
                     <div class="tarjeta-body">
@@ -54,6 +22,7 @@ if ($permiso == '1') {
                 </div>
             </div>
 
+            <!-- Tarjeta 3 -->
             <div class="col-12 col-md-6 col-lg-4">
                 <div class="tarjeta tarjeta-hover shadow-lg border-0 text-center">
                     <div class="tarjeta-body">
@@ -63,6 +32,9 @@ if ($permiso == '1') {
                 </div>
             </div>
 
+            <div class="w-100 d-none d-lg-block"></div>
+
+            <!-- Tarjeta 4 -->
             <div class="col-12 col-md-6 col-lg-4">
                 <div class="tarjeta tarjeta-hover shadow-lg border-0 text-center">
                     <div class="tarjeta-body">
@@ -72,6 +44,7 @@ if ($permiso == '1') {
                 </div>
             </div>
 
+            <!-- Tarjeta 5 -->
             <div class="col-12 col-md-6 col-lg-4">
                 <div class="tarjeta tarjeta-hover shadow-lg border-0 text-center">
                     <div class="tarjeta-body">
@@ -84,7 +57,3 @@ if ($permiso == '1') {
         </div>
     </div>
 </div>
-
-
-
-</html>
