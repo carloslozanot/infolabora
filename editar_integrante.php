d97bd9bebe715ae5c01d3ad078d33d096adf88ca
carloslozanot <carlos.lozano@databiz.co>
2025-08-12 08:27:15 -0500

    Update editar_integrante.php

--- editar_integrante.php
+++ editar_integrante.php
@@ -99,6 +99,11 @@ if (isset($_GET['id'])) {
             </div>
         </div>
     </div>
+    <div class="container my-2">
+        <div class="boton-certificado-lab d-flex justify-content-center">
+            <a href="index_integrante.php" class="btn btn-danger">Regresar</a>
+        </div>
+    </div>
 </div>
 
 </html>
